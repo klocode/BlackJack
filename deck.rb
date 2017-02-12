bf314e340903b74a3b5afeb9d0063f9417113731
klocode <kendrick.t.lo@gmail.com>
2017-02-11 19:33:33 -0500

    minor changes, took out shoe class to make new file

--- deck.rb
+++ deck.rb
@@ -6,11 +6,7 @@ class Deck
 
   def initialize
     @cards = []
-    Card.suits.each do |s|
-      Card.faces.each do |f|
-        cards << Card.new(f, s)
-      end
-    end
+    deal
     shuffle!
   end
 
@@ -18,9 +14,16 @@ class Deck
     cards.shift
   end
 
+  def deal
+    Card.suits.each do |s|
+      Card.faces.each do |f|
+        cards << Card.new(f, s)
+      end
+    end
+  end
+
   def shuffle!
     cards.shuffle!
   end
-
 
 end
