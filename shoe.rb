3ae8097b51d3d1312319e6ec7c55d6b888ce401e
Michael Stashevsky <mstashev@gmail.com>
2017-02-13 12:51:12 -0500

    Added testing for deck class

--- shoe.rb
+++ shoe.rb
@@ -4,11 +4,13 @@ class Shoe < Deck
   #changed create to deal so super looks for
   #the correct method
   def deal
-    #set var shoe = to 7.times so we can
-    #shuffle decks together
-    shoe = 7.times { super }
-    shoe.shuffle!
+    7.times { super }
+    # shoe.shuffle!
   end
+
+  # def shuffle!
+  #   cards.shuffle!
+  # end
 end
 
 # creates 7 decks and stores them in the deck array in blackjack.rb
